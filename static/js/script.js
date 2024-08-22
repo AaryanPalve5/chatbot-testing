47f5891f66ca685a5b586e95a2973b04f3abb491
AaryanPalve5 <aspalve2004@gmail.com>
2024-08-22 17:23:50 +0530

    commit -3

--- static/js/script.js
+++ static/js/script.js
@@ -2,17 +2,25 @@ $(document).ready(function(){
     $("#jokeForm").on("submit", function(event){
         event.preventDefault();
 
-        var user_input = $("#user_input").val();
+        var user_input = $("#user_input").val().trim();
+
+        if (!user_input) {
+            $("#response").text("Please enter a valid input.");
+            return;
+        }
+
+        $("#response").text("Loading...");
 
         $.ajax({
             url: "/",
             type: "POST",
             data: { user_input: user_input },
+            contentType: "application/x-www-form-urlencoded",
             success: function(response){
                 $("#response").text(response.response);
             },
-            error: function(){
-                $("#response").text("An error occurred, please try again.");
+            error: function(xhr, status, error){
+                $("#response").text("Error: " + xhr.responseText);
             }
         });
     });
